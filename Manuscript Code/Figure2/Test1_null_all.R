2660431477b5c62a9af4dab83ca3b24e598444c4
Peter Chi <peter.chi@villanova.edu>
2020-05-21 11:00:07 -0400

    Add files via upload
    needed to change this analysis from quantile to mean (test=7 instead of test=6)

--- Manuscript Code/Figure2/Test1_null_all.R
+++ Manuscript Code/Figure2/Test1_null_all.R
@@ -1,24 +1,26 @@
 #####################################
 # Radius 9
 #
+# 7/5/19 in progress
 
-setwd("...")
+# setwd("C:/Users/pchi01/Dropbox/Temple/Clustering/analyses/Manuscript/April2019")
+setwd("/home/peter/ClusterSignificance/Manuscript/Test1")
 
-source(".../evolClustR/R/countneighbors.R")
-source(".../evolClustR/R/fractions.R")
-source(".../evolClustR/R/findneighbors.R")
-source(".../evolClustR/R/finddist.R")
-source(".../evolClustR/R/readdssp.R")
-source(".../evolClustR/R/generatesasasubs.R")
-source(".../evolClustR/R/parsePDB.R")
-source(".../evolClustR/R/calcsasa.R")
-source(".../evolClustR/R/tailproptest.R")
-source(".../evolClustR/R/fractions_subsonly.R")
-source(".../evolClustR/R/permuteone.R")
-source(".../evolClustR/R/permute.R")
-source(".../evolClustR/R/permutetest.R")
-source(".../evolClustR/R/yutest.R")
-source(".../evolClustR/R/countsubs.R")
+source("/home/peter/ClusterSignificance/evolClustR/R/countneighbors.R")
+source("/home/peter/ClusterSignificance/evolClustR/R/fractions.R")
+source("/home/peter/ClusterSignificance/evolClustR/R/findneighbors.R")
+source("/home/peter/ClusterSignificance/evolClustR/R/finddist.R")
+source("/home/peter/ClusterSignificance/evolClustR/R/readdssp.R")
+source("/home/peter/ClusterSignificance/evolClustR/R/generatesasasubs.R")
+source("/home/peter/ClusterSignificance/evolClustR/R/parsePDB.R")
+source("/home/peter/ClusterSignificance/evolClustR/R/calcsasa.R")
+source("/home/peter/ClusterSignificance/evolClustR/R/tailproptest.R")
+source("/home/peter/ClusterSignificance/evolClustR/R/fractions_subsonly.R")
+source("/home/peter/ClusterSignificance/evolClustR/R/permuteone.R")
+source("/home/peter/ClusterSignificance/evolClustR/R/permute.R")
+source("/home/peter/ClusterSignificance/evolClustR/R/permutetest.R")
+source("/home/peter/ClusterSignificance/evolClustR/R/yutest.R")
+source("/home/peter/ClusterSignificance/evolClustR/R/countsubs.R")
 
 
 Calphas <- parsePDB("2i0q.pdb", subunit="A")
@@ -34,9 +36,10 @@ Calphas <- calc.sasa(Calphas, dssp)
 set.seed(572019)
 # Generate null
 
+# all this needs to be changed/fixed, still many remnants from parametric bootstrap
 perm.reps <- 1000
 
-reps<-1000 
+reps<-1000 # ultimately want 1000
 quantile.p.65.subs <- rep(NA, reps)
 quantile.p.9.subs <- rep(NA, reps)
 quantile.p.115.subs <- rep(NA, reps)
@@ -44,17 +47,20 @@ yu.p <- rep(NA, reps)
 
 for(l in 1:reps){
   subs <- sample(Calphas$pos, 20)
-
-  quantile.p.65.subs[l] <- permute.test(Calphas, subs, reps=perm.reps, radius=6.5, contact=FALSE, test=6)
-  quantile.p.9.subs[l] <- permute.test(Calphas, subs, reps=perm.reps, radius=9, contact=FALSE, test=6)
-  quantile.p.115.subs[l] <- permute.test(Calphas, subs, reps=perm.reps, radius=11.5, contact=FALSE, test=6)
+  # perm.subs <- permute(Calphas, subs, reps=perm.reps, radius=9, contact=TRUE, similarity=1)
+  # I don't know why this was here
+  
+  quantile.p.65.subs[l] <- permute.test(Calphas, subs, reps=perm.reps, radius=6.5, contact=FALSE, test=7)
+  quantile.p.9.subs[l] <- permute.test(Calphas, subs, reps=perm.reps, radius=9, contact=FALSE, test=7)
+  quantile.p.115.subs[l] <- permute.test(Calphas, subs, reps=perm.reps, radius=11.5, contact=FALSE, test=7)
   yu.p[l] <- yu.test(Calphas, subs, perm.reps)
 
   print(l)
+save.image("/home/peter/ClusterSignificance/Manuscript/FinalCode/Figure2_mean/test1_null_all.RData")
 }
 
 
-save.image(".../test1_null_all.RData")
+save.image("/home/peter/ClusterSignificance/Manuscript/FinalCode/Figure2_mean/test1_null_all.RData")
 
 
 
